fde7a815d3b26d0ca18f1e6554ce0552cd8aef9c
Yu Huiyeol <iverlandth@yandex.com>
2017-03-01 19:23:49 -0400

    configure db & migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 # third-party imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 # local imports
 from config import app_config
 
 # db variable initialization
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app(config_name):
@@ -15,9 +18,12 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    # temporary route
-    @app.route('/')
-    def hello_world():
-        return '<h1>Hello, world</h1>';
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+
+    migrate = Migrate(app, db)
+
+    from app import models
 
     return app
--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,25 @@
+import os
+
+try:
+    # localhost connection
+    database = 'eclinical_dev'
+    user = 'postgres'
+    password = 'root'
+    host = 'localhost'
+    port = 5433
+
+except ImportError:
+    # production connection
+    import urlparse
+
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["HEROKU_POSTGRESQL_BRONZE_URL"])
+    database = url.path[1:]
+    user = url.username
+    password = url.password
+    host = url.hostname
+    port = url.port
+
+
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = "postgresql://localhost/eclinical_dev"
+SQLALCHEMY_DATABASE_URI = 'postgres://%s:%s@%s:%s/%s' % (user, password, host, port, database)
